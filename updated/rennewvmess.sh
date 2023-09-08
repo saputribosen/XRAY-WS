70308a1123356a4b1680899097134c3c654675a0
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-09-08 23:59:33 +0700

    Update rennewvmess.sh

--- updated/rennewvmess.sh
+++ updated/rennewvmess.sh
@@ -68,7 +68,8 @@ d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "/### $user $exp/c\\### $user $exp4" $lokasi
+sed -i "/#&# $user $exp/c\\#&# $user $exp4" $lokasi
+sed -i "/Expired  : $exp/c\\Expired  : $exp4" $lok
 systemctl restart xray.service
 service cron restart
 clear
