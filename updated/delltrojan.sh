efbc67b2c44b87750f4da07449846763cf795ee8
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-09-09 16:14:10 +0700

    Update delltrojan.sh

--- updated/delltrojan.sh
+++ updated/delltrojan.sh
@@ -50,7 +50,7 @@ exp=$(grep -E "^#&# " "/etc/xray/config.json" | sort | uniq | cut -d ' ' -f 3 | 
 sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/config.json
 sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/config.json
 rm -f /etc/xray/vmess-$user-tls.json /etc/xray/vmess-$user-nontls.json
-rm -f /usr/bin/trojan/trojangrpc-$user.txt
+rm -f /usr/bin/trojan/trojan-$user.txt | /usr/bin/trojan/trojangrpc-$user.txt
 systemctl restart xray.service
 clear
 echo " "
