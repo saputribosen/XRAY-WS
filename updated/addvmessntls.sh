e500c1d8ae57ef134a6dd981b2c39c2d1069c64e
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-10-09 15:27:48 +0700

    Update addvmessntls.sh

--- updated/addvmessntls.sh
+++ updated/addvmessntls.sh
@@ -45,6 +45,8 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 	done
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (Days) : " masaaktif
+sleep 2
+echo -e "[ ${green}TUNGGU${NC} ] Akun sedang dibuat... "
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmess$/a\### '"$user $exp"'\
