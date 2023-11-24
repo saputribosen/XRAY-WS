57d9c1f4de6d779e27065d9a7287658cc5147f90
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-11-25 00:50:08 +0700

    Update akses.sh

--- updated/akses.sh
+++ updated/akses.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
 
 # Path to the log file
 log_file="/var/log/xray/access.log"
-
-# Extract unique admin emails
 unique_admins=$(awk '/email:/{print $NF}' "$log_file" | sort -u)
 
 # Display the results
 for admin_email in $unique_admins; do
-    echo "Username: $admin_email"
+    echo -e "${NC}${GREEN}User: $admin_email ${NC}"
     echo "-----------------------------------"
     grep -w "email: $admin_email" "$log_file" | grep "accepted" | awk '{split($3, a, ":"); print a[1]}' | sort -u
     echo
