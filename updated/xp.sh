643c75ca9d9a20f5e125eed64d2b624b0e0d2922
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-11-25 15:40:27 +0700

    Update xp.sh

--- updated/xp.sh
+++ updated/xp.sh
@@ -15,14 +15,14 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$( curl https://raw.githubusercontent.com/saputribosen/scriptfree/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Opindoo"
-exit 0
-fi
+#if [ $MYIP = $MYIP ]; then
+#echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+#else
+#echo -e "${NC}${RED}Permission Denied!${NC}";
+#echo -e "${NC}${LIGHT}Please Contact Admin!!"
+#echo -e "${NC}${LIGHT}Telegram : https://t.me/Opindoo"
+#exit 0
+#fi
 
 # CODE AKUN XRAY DI /etc/xray/config.json
 # ====================
