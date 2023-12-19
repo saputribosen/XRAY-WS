ee0834da0c6457d6261babe87641e1bdc426e194
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-12-19 15:46:08 +0700

    Update viewvmess.sh

--- updated/viewvmess.sh
+++ updated/viewvmess.sh
@@ -60,21 +60,6 @@ fi
 systemctl restart xray.service
 echo " "
 echo -e ""
-echo -e "$BLUE╔═══════════════════════════════════════$BLUE╗"
-echo -e "$BLUE╠➣$NC 0$NC. Back to Menu Xray           $BLUE      ║ "
-echo -e "$BLUE║---------------------------------------║"
-echo -e "$BLUE╠➣$NC Script Menu by ARYO                  $BLUE║"
-echo -e "$BLUE╠➣$NC Telegram https://t.me/Opindoo        $BLUE║"
-echo -e "$BLUE╚═══════════════════════════════════════╝$NC"  
-read -p " ➣ Select From Options [ 0 ]:  " menu
 echo -e ""
-case $menu in
-0)
-maddxray
-;;
-*)
-clear
-bash menu
-;;
-esac
+read -sp " Press ENTER to go back"
 #
