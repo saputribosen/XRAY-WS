f154da1d28bc4b15b4862ea5d55ddbb0c4a25b46
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-12-19 16:28:09 +0700

    Update dellvmess.sh

--- updated/dellvmess.sh
+++ updated/dellvmess.sh
@@ -64,3 +64,17 @@ echo "Username  : $user"
 echo "Expired   : $exp"
 echo "==============================="
 echo "Scipt By ARYO"
+echo " "
+read -p " ➣ Select [ 0 BACK Account | Enter To Menu ]:  " menu
+echo -e ""
+
+case $menu in
+0)
+dellvmess
+;;
+  *)
+    clear
+    bash menu
+    ;;
+esac
+#
