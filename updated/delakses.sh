67fd964a9f5a40c608a491b1341db124e849ef29
sarwenda <142522186+saputribosen@users.noreply.github.com>
2024-01-20 01:53:04 +0700

    Update delakses.sh

--- updated/delakses.sh
+++ updated/delakses.sh
@@ -16,20 +16,25 @@ unique_admins=$(awk '/email:/{print $NF}' "$log_file" | sort -u)
 # Kecualikan Tes ping
 excluded_domains="www\.gstatic\.com:80\|bing\.com:80\|cp\.cloudflare\.com:80\|ping\.xmbb\.net:80"
 
-# Tampilkan
+# Cek akun multilogin
 for admin_email in $unique_admins; do
     # Cek apakah ada hasil yang sesuai
     if grep -w "email: $admin_email" "$log_file" | grep "accepted" | grep -v "$excluded_domains" > /dev/null; then
-    
+        echo -e "${NC}Cek: ${GREEN} $admin_email ${NC}"
+        echo ""
+        
         # Hitung jumlah alamat IP unik
         ip_count=$(grep -w "email: $admin_email" "$log_file" | grep "accepted" | grep -v "$excluded_domains" | awk '{split($3, a, ":"); if($3 ~ /tcp/) print a[2]; else print a[1]}' | sort -u | wc -l)
         
         # Tampilkan dan perbarui config.json jika jumlah IP lebih dari 5
         if [ "$ip_count" -gt 5 ]; then
-            sed -i "/\"email\": \"$admin_email\"/ s/\(},\)/#},/" "$config_file" 
+            sed -i "/\"email\": \"$admin_email\"/ s/\(},\)/#},/" "$config_file"
+            echo -e "${NC}Penggunaan Akun Bar-bar"
+            #tambahkan script bot telegram jika ada
         fi    
 
     fi
-clear
+sleep 1
 systemctl restart xray
+clear
 done
