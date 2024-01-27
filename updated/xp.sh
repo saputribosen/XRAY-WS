219bdc15d43b74530e718712600c3fbbef09185f
sarwenda <142522186+saputribosen@users.noreply.github.com>
2024-01-27 17:16:39 +0700

    Update xp.sh

--- updated/xp.sh
+++ updated/xp.sh
@@ -33,78 +33,75 @@ IZIN=$( curl https://raw.githubusercontent.com/saputribosen/scriptfree/main/ipvp
 # ====================
 clear
 systemctl restart xray
-# Delete Vmess
-datavm=($(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sort | uniq | column -t))
 now=$(date +"%Y-%m-%d")
-for user in "${datavm[@]}"; do
-    expv=$(grep -w "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)   
-    # Cek apakah tanggal 'expv' valid sebelum mencoba mengonversinya
-    if date -d "$expv" > /dev/null 2>&1; then
-        d1=$(date -d "$expv" +%s)
-    else
-        echo "Tanggal tidak valid untuk user $user: $expv"
-        continue  # Lanjutkan dengan pengguna berikutnya
-    fi
-    d2=$(date -d "$now" +%s)
-    exp1=$(( (d1 - d2) / 86400 ))
-    
-    if [[ "$exp1" -lt 0 ]]; then
-        sed -i "/^### $user $expv/,/^},{/d" "/etc/xray/config.json"
-        rm -f "/etc/xray/vmess-$user-tls.json" "/etc/xray/vmess-$user-nontls.json" "/etc/xray/vmess-$user-grpc.json"
-        rm -f "/usr/bin/vmess/vmess-$user.txt" "/usr/bin/vmess/vmess-$user-ntls.txt" "/usr/bin/vmess/vmess-$user-grpc.txt"
+config_file="/etc/xray/config.json"
+# Delete Vmess
+grep "^### " "$config_file" | while read -r line; do
+    if [[ "$line" =~ ^"### " ]]; then
+        
+        user=$(echo "$line" | awk '{print $2}')
+        expiration_date=$(echo "$line" | awk '{print $3}')
+
+        # Check if the expiration date is today
+        if [[ "$expiration_date" == "$now" ]]; then
+            # Remove the user configuration
+            sed -i "/^### $user/,/},{/d" "$config_file"
+
+            telegram "User $user has been removed. (Vmess)"
+        fi
     fi
 done
 sleep 1
 # Delete trojan
-datatj=($(grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 2 | sort | uniq | column -t))
-now=$(date +"%Y-%m-%d");
-for user in "${datatj[@]}"; do
- exp=$(grep -w "^#&# $user" /etc/xray/config.json | cut -d ' ' -f 3 | sort | uniq);
-    # Cek apakah tanggal 'exp' valid sebelum mencoba mengonversinya
-    if date -d "$exp" > /dev/null 2>&1; then
-        d1=$(date -d "$exp" +%s)
-    else
-        echo "Tanggal tidak valid untuk user $user: $exp"
-        continue  # Lanjutkan dengan pengguna berikutnya
+grep "^#&# " "$config_file" | while read -r line; do
+    if [[ "$line" =~ ^"#&# " ]]; then
+        
+        user=$(echo "$line" | awk '{print $2}')
+        expiration_date=$(echo "$line" | awk '{print $3}')
+
+        # Check if the expiration date is today
+        if [[ "$expiration_date" == "$now" ]]; then
+            # Remove the user configuration
+            sed -i "/^#&# $user/,/},{/d" "$config_file"
+
+            telegram "User $user has been removed. (Trojan)"
+        fi
     fi
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
- if [[ "$exp2" -lt 0 ]]; then
-  sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/config.json
-  rm -f "/usr/bin/trojan/trojan-$user.txt" "/usr/bin/trojan/trojan-$user-grpc.txt"
- fi
 done
 sleep 1
 # Delete Vless
-data=( $(cat /etc/xray/config.json | grep '^####' | cut -d ' ' -f 2) );
-now=$(date +"%Y-%m-%d");
-for user in "${data[@]}"; do
- exp=$(grep -w "^#### $user" /etc/xray/config.json | cut -d ' ' -f 3 | sort | uniq);
- d1=$(date -d "$exp" +%s);
- d2=$(date -d "$now" +%s);
- exp2=$(( (d1 - d2) / 86400 ));
- if [[ "$exp2" = "0" ]]; then
-   sleep 1
-   sed -i "/^#### $user $exp/,/^},{/d" /etc/xray/config.json
-   rm -f /usr/bin/vless/vless-$user.txt
- fi
+grep "^#### " "$config_file" | while read -r line; do
+    if [[ "$line" =~ ^"#### " ]]; then
+        
+        user=$(echo "$line" | awk '{print $2}')
+        expiration_date=$(echo "$line" | awk '{print $3}')
+
+        # Check if the expiration date is today
+        if [[ "$expiration_date" == "$now" ]]; then
+            # Remove the user configuration
+            sed -i "/^#### $user/,/},{/d" "$config_file"
+
+            telegram "User $user has been removed. (Vless)"
+        fi
+    fi
 done
 sleep 1
 # Delete shadowsocks
-data=( $(cat /etc/xray/config.json | grep '^##&#' | cut -d ' ' -f 2) );
-now=$(date +"%Y-%m-%d");
-for user in "${data[@]}"; do
- exp=$(grep -w "^##&# $user" /etc/xray/config.json | cut -d ' ' -f 3 | sort | uniq);
- d1=$(date -d "$exp" +%s);
- d2=$(date -d "$now" +%s);
- exp2=$(( (d1 - d2) / 86400 ));
- if [[ "$exp2" = "0" ]]; then
-  sleep 1
-  sed -i "/^##&# $user $exp/,/^},{/d" /etc/xray/config.json
-  rm -f /home/vps/public_html/ss-ws-$user.txt
-  rm -f /home/vps/public_html/ss-grpc-$user.txt
-  rm -f /usr/bin/shadowsock/ss-$user.txt
- fi
+grep "^##&# " "$config_file" | while read -r line; do
+    if [[ "$line" =~ ^"##&# " ]]; then
+        
+        user=$(echo "$line" | awk '{print $2}')
+        expiration_date=$(echo "$line" | awk '{print $3}')
+
+        # Check if the expiration date is today
+        if [[ "$expiration_date" == "$now" ]]; then
+            # Remove the user configuration
+            sed -i "/^##&# $user/,/},{/d" "$config_file"
+
+            telegram "User $user has been removed. (shadowsock)"
+        fi
+    fi
 done
 sleep 1
 systemctl restart xray
+menu
