38ea4fb8b183825288d2024c549637aa6e4a4e6f
sarwenda <142522186+saputribosen@users.noreply.github.com>
2024-01-29 20:51:08 +0700

    Update install-xray.sh

--- install-xray.sh
+++ install-xray.sh
@@ -110,7 +110,7 @@ mkdir -p /home/vps/public_html
 # set Cron
 echo "59 */4 * * * /usr/bin/restart-xray" >> /var/spool/cron/crontabs/root
 echo "59 23 * * * /usr/bin/xp" >> /var/spool/cron/crontabs/root
-echo "*/2 * * * * /usr/bin/delakses" >> /var/spool/cron/crontabs/root
+echo "*/5 * * * * /usr/bin/delakses" >> /var/spool/cron/crontabs/root
 echo "0 0 * * * /usr/bin/xpakses" >> /var/spool/cron/crontabs/root
 #echo "57 23 * * * reboot" >> /etc/crontab
 systemctl restart cron
