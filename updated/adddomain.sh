489e0f589a19a0aefa01ce209bfc9f83d3180031
sarwenda <142522186+saputribosen@users.noreply.github.com>
2024-02-25 19:12:03 +0700

    Update adddomain.sh

--- updated/adddomain.sh
+++ updated/adddomain.sh
@@ -77,7 +77,7 @@ echo "IP=$domain1" >> /var/lib/airavpn/ipvps.conf
 clear
 sleep 1
 echo -e "Upgrade ${NC}Please Wait..."
-apt update -y && apt upgrade -y && update-grub
+apt update -y
 #Install Kelengkapan
 apt install wget curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
