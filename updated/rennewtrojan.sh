1db0235a56b9b1ae156fdea690ca9fa7dfbc3b49
sarwenda <142522186+saputribosen@users.noreply.github.com>
2025-01-19 11:29:25 +0700

    Update rennewtrojan.sh

--- updated/rennewtrojan.sh
+++ updated/rennewtrojan.sh
@@ -70,8 +70,6 @@ exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
 sed -i "/#&# $user $exp/c\\#&# $user $exp4" $lokasi
 sed -i "/Expired  : $exp/c\\Expired  : $exp4" $lok || sed -i "/Expired  : $exp/c\\Expired  : $exp4" $lokgrpc
-systemctl restart xray.service
-service cron restart
 clear
 echo ""
 echo "==============================="
@@ -81,6 +79,10 @@ echo "Username  : $user"
 echo "Expired   : $exp4"
 echo "==============================="
 echo " "
+sleep 5
+
+systemctl restart xray.service
+service cron restart
 read -p " ➣ Select 0 BACK Renew | Enter To Menu :  " menu
 echo -e ""
 
